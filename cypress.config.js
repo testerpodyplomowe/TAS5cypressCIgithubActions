b103494d3cd115a6be2a24f87eaac3a99fb1ea16
testerpodyplomowe <testerpodyplomowe@gmail.com>
2026-05-31 15:42:25 +0200

    update workflow with cypress cloud dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "nswvvw",
+  projectId: "fmbo4x",
   allowCypressEnv: false,
   reporter: "cypress-mochawesome-reporter",
   reporterOptions: {
